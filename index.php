c9593fd6cd5b828a51de1400f01bf5996ab85505
web-tech-surgu <95065223+web-tech-surgu@users.noreply.github.com>
2022-04-02 14:08:55 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
-<?
-  echo "Hello World"
-   ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title> Лабораторная работа 1. </title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+  <div id="categories">
+       Блок со списком категорий
+  </div>
+    <div id="container">
+     <div id="objects">
+	   Блок со списками объектов
+	 </div>
+	 <div id="content">
+	   Блок с описание объектов
+	 </div>
+  </div>
+</body>
+</html> 
+	 
