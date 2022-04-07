cc3673636c7326cae964f490d5b7b518312c7177
web-tech-surgu <95065223+web-tech-surgu@users.noreply.github.com>
2022-04-07 09:25:55 +0500

    Update index.php

--- index.php
+++ index.php
@@ -23,24 +23,22 @@
                     case "1": echo(file_get_contents("objects1.html")); break;
                     case "2": echo(file_get_contents("objects2.html")); break;
                     case "3": echo(file_get_contents("objects3.html")); break;
-                    default: echo (file_get_contents("about.html"));
-                }
+                    default: echo (file_get_contents("about.html"));}
             ?>
               </div>
             <div id="content">
-               
-                             <?php
+               <?php
                 $O = $_GET["O"];
                 switch ($O) {
-                    case "1-1": echo (file_get_contents("objects1-1.html")); break;
-                    case "1-2": echo (file_get_contents("objects1-2.html")); break;
-                    case "1-3": echo (file_get_contents("objects1-3.html")); break;
-                    case "2-1": echo (file_get_contents("objects2-1.html")); break;
-                    case "2-2": echo (file_get_contents("objects2-2.html")); break;
-                    case "2-3": echo (file_get_contents("objects2-3.html")); break;
-                    case "3-1": echo (file_get_contents("objects3-1.html")); break;
-                    case "3-2": echo (file_get_contents("objects3-2.html")); break;
-                    case "3-3": echo (file_get_contents("objects3-3.html")); break;
+                    case "1-1": echo (file_get_contents("object1-1.html")); break;
+                    case "1-2": echo (file_get_contents("object1-2.html")); break;
+                    case "1-3": echo (file_get_contents("object1-3.html")); break;
+                    case "2-1": echo (file_get_contents("object2-1.html")); break;
+                    case "2-2": echo (file_get_contents("object2-2.html")); break;
+                    case "2-3": echo (file_get_contents("object2-3.html")); break;
+                    case "3-1": echo (file_get_contents("object3-1.html")); break;
+                    case "3-2": echo (file_get_contents("object3-2.html")); break;
+                    case "3-3": echo (file_get_contents("object3-3.html")); break;
                     case "99": include ("form_LAB6.php"); break;
                     default: echo (file_get_contents("applicate.html"));}   
                 ?>
