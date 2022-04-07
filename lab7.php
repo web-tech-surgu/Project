cbb2688bb4531a52792f1bc0ae0f06a2ce698a21
web-tech-surgu <95065223+web-tech-surgu@users.noreply.github.com>
2022-04-07 11:03:19 +0500

    Update lab7.php

--- lab7.php
+++ lab7.php
@@ -1,29 +1,30 @@
- <div id="контейнер">
-            <div id="объекты">         
- $C = $_GET["C"];
- коммутатор ($C) {
- case "1": $objects=file_get_contents("objects1.php"); перерыв;
- case "2": $objects=file_get_contents("objects2.php"); перерыв;
- case "3": $objects=file_get_contents("objects3.php"); перерыв;
- по умолчанию: $objects=file_get_contents("about.php);
+ <?php
+    $tmp=file_get_contents("index.tpl");
+    $objects="";
+    $contents=""; 
+       $C = $_GET["C"];
+       switch($C) {
+            case "1": $objects=file_get_contents("objects1.php"); break;
+            case "2": $objects=file_get_contents("objects2.php"); break;
+            case "3": $objects=file_get_contents("objects3.php"); break;
+            default: $objects=file_get_contents("about.php);
                 }
-            ?>
-              </div>
-            <div id="content">
-               <?php
-                $O = $_GET["O"];
-                коммутатор ($O) {
-                    case "1-1": echo(file_get_contents("object1-1.php")); перерыв;
-                    case "1-2": echo(file_get_contents("object1-2.php")); перерыв;
-                    case "1-3": echo(file_get_contents("object1-3.php")); перерыв;
-                    case "2-1": echo(file_get_contents("object2-1.php")); перерыв;
-                    case "2-2": echo(file_get_contents("object2-2.php")); перерыв;
-                    case "2-3": echo(file_get_contents("object2-3.php")); перерыв;
-                    case "3-1": echo(file_get_contents("object3-1.php")); перерыв;
-                    case "3-2": echo(file_get_contents("object3-2.php")); перерыв;
-                    case "3-3": echo(file_get_contents("object3-3.php")); перерыв;
-                    по умолчанию: echo (file_get_contents("applicate.html"));
-                }   
-                ?>
-            </div>
-        </div>
+             
+        $O = $_GET["O"];
+                switch ($O) {
+                    case "1-1": $content=file_get_contents("object1-1.php"); break;
+                    case "1-2": $content=file_get_contents("object1-2.php"); break;
+                    case "1-3": $content=file_get_contents("object1-3.php"); break;
+                    case "2-1": $content=file_get_contents("object2-1.php"); break;
+                    case "2-2": $content=file_get_contents("object2-2.php"); break;
+                    case "2-3": $content=file_get_contents("object2-3.php"); break;
+                    case "3-1": $content=file_get_contents("object3-1.php"); break;
+                    case "3-2": $content=file_get_contents("object3-2.php"); break;
+                    case "3-3": $content=file_get_contents("object3-3.php"); break;
+                    default: $content=file_get_contents("applicate.php");
+                }; 
+                $tmp = str_replace("{objects}", $objects, $tmp);
+                $tmp = str_replace("{content}", $objects, $tmp);
+      echo $tmp;
+                
+?>
