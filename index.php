183d7a5df3a15c7bc8e85aa2e45257a94041ad81
web-tech-surgu <95065223+web-tech-surgu@users.noreply.github.com>
2022-04-07 09:17:36 +0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title> Лабораторная работа 6. Блочная верстка сайта</title>
-        <link rel="stylesheet" type="text/css" href="style.scss" />
+        <link rel="stylesheet" type="text/css" href="style.css" />
     </head>
     <body>
         <div id="categories"> 
