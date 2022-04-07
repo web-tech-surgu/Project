02ce96a732175c819722d5bb9eb26c2c466506d0
web-tech-surgu <95065223+web-tech-surgu@users.noreply.github.com>
2022-04-07 10:50:36 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,44 +1,23 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> Лабораторная работа 6. Блочная верстка сайта</title>
+        <title> Лабораторная работа 7. Динамическая генерация веб-страниц на основе шаблона</title>
         <link rel="stylesheet" type="text/css" href="style.css" />
     </head>
     <body>
         <div id="categories"> 
             Категории
-                <a href="index.php?C=1">Категория 1</a>
-                <a href="index.php?C=2">Категория 2</a>
-                <a href="index.php?C=3">Категория 3</a>
-                <a href="index.php">Домой</a>
+                <a href="lab7.php?C=1">Категория 1</a>
+                <a href="lab7.php?C=2">Категория 2</a>
+                <a href="lab7.php?C=3">Категория 3</a>
+                <a href="lab7.php">Домой</a>
         </div>
         <div id="container">
             <div id="objects">         
-                $C = $_GET["C"];
-                switch ($C) {
-                    case "1": $objects=file_get_contents("objects1.php"); break;
-                    case "2": $objects=file_get_contents("objects2.php"); break;
-                    case "3": $objects=file_get_contents("objects3.php"); break;
-                    default: $objects=file_get_contents("about.php");
-                }
-            ?>
+                
               </div>
             <div id="content">
-               <?php
-                $O = $_GET["O"];
-                switch ($O) {
-                    case "1-1": echo(file_get_contents("object1-1.php")); break;
-                    case "1-2": echo(file_get_contents("object1-2.php")); break;
-                    case "1-3": echo(file_get_contents("object1-3.php")); break;
-                    case "2-1": echo(file_get_contents("object2-1.php")); break;
-                    case "2-2": echo(file_get_contents("object2-2.php")); break;
-                    case "2-3": echo(file_get_contents("object2-3.php")); break;
-                    case "3-1": echo(file_get_contents("object3-1.php")); break;
-                    case "3-2": echo(file_get_contents("object3-2.php")); break;
-                    case "3-3": echo(file_get_contents("object3-3.php")); break;
-                    default: echo (file_get_contents("applicate.html"));
-                }   
-                ?>
+              
             </div>
         </div>
         </body>
