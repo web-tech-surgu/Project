082fdc63a76fa0585d5be609c2bbb058b7b4d8bb
web-tech-surgu <95065223+web-tech-surgu@users.noreply.github.com>
2022-04-07 08:57:29 +0500

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
         <div id="categories"> 
             Категории
             <ul id="list">
-                <li><a href="index.php? c=1">Категория 1</a></li>
-                <li><a href="index.php? c=2">Категория 2</a></li>
-                <li><a href="index.php? c=3">Категория 3</a></li>
+                <li><a href="index.php?c=1">Категория 1</a></li>
+                <li><a href="index.php?c=2">Категория 2</a></li>
+                <li><a href="index.php?c=3">Категория 3</a></li>
                 <li><a href="index.php">Домой</a></li>
                 <li><a href="index.php? 0=99">Форма</a></li>
             </ul>
